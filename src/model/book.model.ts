b36de86491d6e26346f2a9569f8d630d39edecd1
HU SHUKANG <hushukang@gmail.com>
2024-12-25 14:57:51 +0900

    change common model

--- src/model/book.model.ts
+++ src/model/book.model.ts
@@ -1,21 +1,18 @@
+import { DBCommon, dbCommonDesc, DBCommonType } from './common.model';
+
 export type Book = {
   id: string;
   title: string;
   content: string;
-  createdAt: string;
-  updatedAt: string;
-  deletedAt?: string;
-};
+} & DBCommon;
 
 export const bookDesc = {
   id: '書籍ID',
   title: '書籍のタイトル',
   content: '書籍の内容',
-  createdAt: '作成日時',
-  updatedAt: '更新日時',
-  deletedAt: '削除日時',
+  ...dbCommonDesc,
 };
 
-export type BookInput = Omit<Book, 'id' | 'createdAt' | 'updatedAt' | 'deletedAt'>;
+export type BookInput = Omit<Book, 'id' | DBCommonType>;
 export type BookIdInput = Pick<Book, 'id'>;
 export type BookQueryInput = Partial<Pick<Book, 'title' | 'content'>>;
--- src/model/common.model.ts
+++ src/model/common.model.ts
@@ -1,1 +1,17 @@
 export type Optional<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+export type Expand<T> = T extends infer O ? { [K in keyof O]: O[K] } : never;
+
+export type DBCommon = {
+  createdAt: string;
+  updatedAt: string;
+  deletedAt?: string;
+};
+
+/** type DBCommonType = 'createdAt' | 'updatedAt' | 'deletedAt'; */
+export type DBCommonType = keyof DBCommon;
+
+export const dbCommonDesc = {
+  createdAt: '作成日時',
+  updatedAt: '更新日時',
+  deletedAt: '削除日時',
+};
