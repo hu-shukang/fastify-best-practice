17c867c69f0c97f3f623382827ca8013de626582
胡书康 <hushukang@gmail.com>
2025-02-07 22:00:42 +0900

    change request-context plugin

--- src/plugin/request-context.global.plugin.ts
+++ src/plugin/request-context.global.plugin.ts
@@ -1,3 +1,4 @@
+import { prisma } from '@/util/prisma.util';
 import fastifyRequestContext from '@fastify/request-context';
 import { FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
@@ -8,6 +9,11 @@ const requestContextPlugin: FastifyPluginAsync = fp(async (fastify) => {
       const reqId = Math.random().toString(36).substring(2, 12);
       const logPrefix = request.routeOptions.config.logPrefix || '';
       const logger = request.log.child({ logPrefix, reqId });
+      prisma.$on('query', (event) => {
+        logger.info(`Prisma Query: ${event.query}`);
+        logger.info(`Prisma Params: ${event.params}`);
+        logger.info(`Prisma Duration: ${event.duration}ms`);
+      });
       return {
         logger: logger,
         reqId: reqId,
--- src/util/prisma.util.ts
+++ src/util/prisma.util.ts
@@ -3,10 +3,4 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient({ log: [{ emit: 'event', level: 'query' }] });
 
-prisma.$on('query', (event) => {
-  logger.info(`Prisma Query: ${event.query}`);
-  logger.info(`Prisma Params: ${event.params}`);
-  logger.info(`Prisma Duration: ${event.duration}ms`);
-});
-
 export { prisma };
